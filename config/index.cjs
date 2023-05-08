a9046127149aa7154d553b9b3f730704890aa6b3
CaoHongTao <71419687+Caohongtao97@users.noreply.github.com>
2023-05-08 20:51:53 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oyymf57FCNDKvoPMlmZiBwjGSCs0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Cq9iNNjGp1obye6DJ4gJbBATXWZKFV-PvZyuU-K-suc',
+      useTemplateId: 'BKHibffHgXtxCeFbFopreltiJspLw7t29ZDwu0fK_Ws',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-26',
       festivals: [
